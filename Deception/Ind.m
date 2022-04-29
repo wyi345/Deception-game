460426b021e44d4da478344792e18695f8655f0d
wyi345 <104217205+wyi345@users.noreply.github.com>
2022-04-29 09:23:41 -0400

    Update Ind.m

--- Deception/Ind.m
+++ Deception/Ind.m
@@ -1,6 +1,6 @@
 function x=Ind(gamma0,pi0,gamma1,pi1,theta)
 global m 
-  if gamma0*pi0>gamma1*pi1%%
+  if gamma0*pi0>gamma1*pi1%%calculate the best response for the receiver
       m = 0;
   else 
       m = 1;
